588ea285d487a5de9f0834e3d6ae0326dc0e6b43
lifr0m <artem.kay@yandex.ru>
2024-06-18 04:46:11 +0300

    Ensure no trailing new line in data files.

--- src/usrnamegen/__init__.py
+++ src/usrnamegen/__init__.py
@@ -6,9 +6,9 @@ _ROOT = Path(__file__).parent
 
 
 def generate_username() -> str:
-    noun_list = (_ROOT / 'words/nouns.txt').read_text().split('\n')
-    adjective_list = (_ROOT / 'words/adjectives.txt').read_text().split('\n')
-    censored = (_ROOT / 'words/blacklist.txt').read_text().split('\n')
+    noun_list = (_ROOT / 'words/nouns.txt').read_text().strip().split('\n')
+    adjective_list = (_ROOT / 'words/adjectives.txt').read_text().strip().split('\n')
+    censored = (_ROOT / 'words/blacklist.txt').read_text().strip().split('\n')
 
     while True:
         adjective = random.choice(adjective_list)
